2ad8bc0f2dec03773db490d27bd11b849ba0bf84
Sara Posada <saraposadadominguez@gmail.com>
2026-05-21 10:29:41 +0100

    Update create.sql

--- sql/create.sql
+++ sql/create.sql
@@ -1,8 +1,8 @@
-CREATE TABLE formsaraposada (
+CREATE TABLE formsaraposada(
 id SERIAL PRIMARY KEY,
 name VARCHAR (100),
-email VARCHAR(255) NOT NULL,
-subject VARCHAR (50) NOT NULL,
+email VARCHAR (100) NOT NULL,
+subject VARCHAR (100) NOT NULL,
 age INTEGER CHECK (age >18),
 message VARCHAR (500) NOT NULL
 );
