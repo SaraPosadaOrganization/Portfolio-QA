f928cf8274f54c7dd1fc6578cd024d51db10897a
Sara Posada <saraposadadominguez@gmail.com>
2026-06-07 09:50:28 +0100

    Update t2e4email.py

---
+++ ejercicios-sara/t2e4email.py
@@ -0,0 +1,11 @@
+# Introduce un correo electrónico
+correo_electrónico: str = input("Introduce una correo electrónico: ")
+
+# Mostrar la longitud
+print("Longitud del correo electrónico:", len(correo_electrónico))
+
+# Mostrar en mayúsculas
+print("En mayúsculas:", correo_electrónico.upper())
+
+# Mostrar en minúsculas
+print("En minúsculas:", correo_electrónico.lower())
