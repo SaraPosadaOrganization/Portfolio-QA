1e090ba57d2aa286e0db960742a652ab9fa0ca5b
Sara Posada <saraposadadominguez@gmail.com>
2026-06-07 17:10:19 +0100

    Update t3e1preciototal.py

--- ejercicios-sara/t3e1preciototal.py
+++ ejercicios-sara/t3e1preciototal.py
@@ -5,13 +5,13 @@ cantidad = int(input("Cantidad a comprar: "))
 descuento = float(input("Descuento (%): "))
 
 # Función que reciba precio, cantidad y descuento y devuelva el precio con descuento aplicado
-def calcular_descuento(precio, cantidad, descuento):
+def calcular_total_descuento(precio, cantidad, descuento):
     total = precio * cantidad
-    total_descuento = total - (total * descuento / 100)
-    return total_descuento
+    total_descuento = total * descuento / 100
+    return total - total_descuento
 
 # Mostramos cantidad, nombre del producto, descuento y precio total con descuento
-precio_final = calcular_descuento(precio, cantidad, descuento)
+precio_final = calcular_total_descuento(precio, cantidad, descuento)
 print("Producto:", nombre)
 print("Cantidad:", cantidad)
 print("Descuento:", descuento, "%")
@@ -24,5 +24,9 @@ def aplicar_iva(cantidad):
 # Mostramos total con IVA aplicado
 total_con_iva = aplicar_iva(precio_final)
 print("Total con IVA:", total_con_iva, "€")
+
+
+
+
 
 
